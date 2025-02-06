a80937c2da331a103d51b79b5e93d0f2191347cf
Ahmadk11 <kaikatiahmad14@gmail.com>
2025-02-06 10:29:57 -0800

    creating dictionary for entrance exam

--- entrance_exam.php
+++ entrance_exam.php
@@ -1,7 +1,27 @@
 <?php 
 
 
-echo "Hello world!";
+$students_exam = [
+    1 => [
+        "id" => "S001",
+        "name" => "Ahmad Khalid",
+        "email" => "ahmad.khalid@example.com",
+        "exam_score" => 85
+    ],
+    2 => [
+        "id" => "S002",
+        "name" => "Sarah Ali",
+        "email" => "sarah.ali@example.com",
+        "exam_score" => 92
+    ],
+    3 => [
+        "id" => "S003",
+        "name" => "Adam Yehya",
+        "email" => "adam.yehya@example.com",
+        "exam_score" => 74
+    ]
+];
+
 
 
 ?>
