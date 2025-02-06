bf7fe262816819ef1c376f177fc13911403091f5
Ahmadk11 <kaikatiahmad14@gmail.com>
2025-02-06 03:41:42 -0800

    saving and creating class Participants

--- participants.php
+++ participants.php
@@ -1,5 +1,22 @@
 <?php
 
-echo "hello world!";
+class participants {
+
+    private $id;
+    private $name;
+    private $email;
+    private $profile_picture;
+    private $batch;
+    private $graduation_status;
+
+    public function __construct($id, $name, $email, $profile_picture, $batch, $graduation_status=false){
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->profile_picture = $profile_picture;
+        $this->batch = $batch;
+        $this->graduation_status = $graduation_status;
+    }
+}
 
 ?>
